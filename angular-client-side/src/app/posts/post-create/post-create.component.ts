a19ba1b49c5d8867b8eb180181bb28793057506b
dejan-krstic <dejanmkrstic@gmail.com>
2018-11-29 21:34:06 +0100

    Posts pipelined through post service

--- angular-client-side/src/app/posts/post-create/post-create.component.ts
+++ angular-client-side/src/app/posts/post-create/post-create.component.ts
@@ -1,8 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { ClassGetter } from '@angular/compiler/src/output/output_ast';
-import { Post } from '../post.model';
+import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-create',
@@ -12,17 +11,14 @@ import { NgForm } from '@angular/forms';
 export class PostCreateComponent {
   enteredTitle = '';
   enteredContent = '';
-  @Output() postCreated = new EventEmitter<Post>();
+
+  constructor(public postsService: PostsService) {}
 
   onAddPost(form: NgForm) {
     if (form.invalid) {
-      // return;
+      return;
     }
-    const post: Post = {
-      title: form.value.title,
-      content: form.value.content
-    };
-    this.postCreated.emit(post);
-
+    this.postsService.addPost(form.value.title, form.value.content);
+    form.resetForm();
   }
 }
